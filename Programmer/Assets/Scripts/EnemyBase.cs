75808f80b31f5e84e1ea212d49aaba1a3da080e4
Jeremy Buentello <jb09056@georgiasouthern.edu>
2022-08-09 00:53:23 -0400

    Fixing up the enemy class

--- Programmer/Assets/Scripts/EnemyBase.cs
+++ Programmer/Assets/Scripts/EnemyBase.cs
@@ -1,21 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
     public float Speed = 2.0f;
+    public float Range = 15;
     private Trans _target;
+
+    public EventHandler OnDisable;
 
     private void OnEnable()
     {
-        var xCoor = Random.Range(-5, 5);
+        var xCoor = UnityEngine.Random.Range(-1 * Range, Range);
         transform.position = new Vector3(xCoor, transform.position.y, transform.position.z);
         _target = new Trans(transform);
-        _target.position += new Vector3(xCoor, 0, -300);
+        _target.position += new Vector3(xCoor, 0, -120);
     }
 
     private void Update()
     {
         var step = Speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, _target.position, step);
+
+        if (Vector3.Distance(transform.position, _target.position) < 0.1f)
+        {
+            OnDisable?.Invoke(this, EventArgs.Empty);
+            enabled = false;
+        }
     }
 }
